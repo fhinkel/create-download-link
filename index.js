db64c69b6b693378ee32cf8fe66be04cea27371b
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2016-04-13 11:38:31 -0400

    Refactor index.js

--- index.js
+++ index.js
@@ -3,23 +3,26 @@ module.exports = function(opt) {
   var title = opt.title;
   var filename = opt.filename;
 
+  var anchor = document.createElement('a');
+
   // creates
   // <a href="data:application/octet-stream,DATA" download="FILENAME">TITLE</a>
-
-  var createDownloadableFileLink = function(data, filename) {
+  var attachDataAsOctetStream = function(data) {
     var link = 'data:application/octet-stream,' + encodeURIComponent(data);
-    var anchor = document.createElement('a');
     anchor.setAttribute('href', link);
+  };
+
+  var setFilename = function(filename) {
     anchor.setAttribute('download', filename);
-    return anchor;
   };
 
-  var setTitleTo = function(anchor, title) {
+  var setTitle = function(title) {
     anchor.appendChild(document.createTextNode(title));
-    return anchor;
   };
 
-  var anchor = createDownloadableFileLink(data, filename);
-  anchor = setTitleTo(anchor, title);
+  attachDataAsOctetStream(data);
+  setFilename(filename);
+  setTitle(title);
+
   return anchor;
 };
