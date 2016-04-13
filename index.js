7b6f55f0ff4b499a1bb40d6141a4348f5f01fbd8
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2016-04-13 10:38:15 -0400

    Add ESLint with google style, so stylish!

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-module.exports = function (opt) {
-
-    var data = opt.data;
-    var title = opt.title;
-    var filename = opt.filename;
+module.exports = function(opt) {
+  var data = opt.data;
+  var title = opt.title;
+  var filename = opt.filename;
 
     // creates
     // <a href="data:application/octet-stream,DATA" download="FILENAME">TITLE</a>
 
-    var createDownloadableFileLink = function (data, filename) {
-        var link = 'data:application/octet-stream,' + encodeURIComponent(data);
-        var anchor = document.createElement('a');
-        anchor.setAttribute('href', link);
-        anchor.setAttribute('download', filename);
-        return anchor;
-    };
-
-    var setTitleTo = function(anchor, title) {
-        anchor.appendChild(document.createTextNode(title));
-        return anchor;
-    };
-
-    var anchor = createDownloadableFileLink(data, filename);
-    anchor = setTitleTo(anchor, title);
+  var createDownloadableFileLink = function(data, filename) {
+    var link = 'data:application/octet-stream,' + encodeURIComponent(data);
+    var anchor = document.createElement('a');
+    anchor.setAttribute('href', link);
+    anchor.setAttribute('download', filename);
     return anchor;
+  };
+
+  var setTitleTo = function(anchor, title) {
+    anchor.appendChild(document.createTextNode(title));
+    return anchor;
+  };
+
+  var anchor = createDownloadableFileLink(data, filename);
+  anchor = setTitleTo(anchor, title);
+  return anchor;
 };
--- test.js
+++ test.js
@@ -1,42 +1,40 @@
 var assert = require('chai').assert;
 var createDownloadLink = require('./index.js');
 
-describe('When you invoke create-download-link,', function () {
+describe('When you invoke create-download-link,', function() {
+  before(function() {
+    var opt = {
+      data: 'nice data',
+      title: 'nice title',
+      filename: 'filename.txt'
+    };
+    this.anchor = createDownloadLink(opt);
+  });
 
-    before(function () {
-        var opt = {
-            data: 'nice data',
-            title: 'nice title',
-            filename: 'filename.txt'
-        };
-        this.anchor = createDownloadLink(opt);
+  describe('the link', function() {
+    it('should be of type anchor', function() {
+      assert.equal(this.anchor.nodeName, 'A');
     });
-
-    describe('the link', function () {
-        it('should be of type anchor', function () {
-            assert.equal(this.anchor.nodeName, 'A');
-        });
-        it('should have the correct attributes', function () {
-            assert.equal(this.anchor.getAttribute('href'), 'data:application/octet-stream,nice%20data');
-            assert.equal(this.anchor.getAttribute('download'), 'filename.txt');
-        });
-        it('should have a title', function () {
-            assert.equal(this.anchor.childNodes.length, 1);
-            assert.equal(this.anchor.firstChild.nodeValue, 'nice title');
-        });
+    it('should have the correct attributes', function() {
+      assert.equal(this.anchor.getAttribute('href'),
+          'data:application/octet-stream,nice%20data');
+      assert.equal(this.anchor.getAttribute('download'),
+          'filename.txt');
     });
-    describe('creation', function () {
-        it('should fail if data is missing', function () {
-
-        });
-        it('should fail if data is missing', function () {
-
-        });
-        it('should fail if data is missing', function () {
-
-        });
+    it('should have a title', function() {
+      assert.equal(this.anchor.childNodes.length, 1);
+      assert.equal(this.anchor.firstChild.nodeValue, 'nice title');
+    });
+  });
+  describe('creation', function() {
+    it('should fail if data is missing', function() {
 
     });
+    it('should fail if data is missing', function() {
+
+    });
+    it('should fail if data is missing', function() {
+
+    });
+  });
 });
-
-
